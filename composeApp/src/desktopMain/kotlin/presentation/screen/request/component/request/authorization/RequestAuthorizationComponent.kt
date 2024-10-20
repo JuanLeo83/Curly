cc1448452e575238de4dea9373f797a2363735b1
Juan Leo <juanleodev@gmail.com>
2024-10-20 11:29:16 +0200

    Fix coming soon style

--- composeApp/src/desktopMain/kotlin/presentation/screen/request/component/request/authorization/RequestAuthorizationComponent.kt
+++ composeApp/src/desktopMain/kotlin/presentation/screen/request/component/request/authorization/RequestAuthorizationComponent.kt
@@ -1,15 +1,21 @@
 package presentation.screen.request.component.request.authorization
 
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import domain.model.AuthorizationType
 import presentation.common.component.dropdown.DropdownComponent
 import presentation.screen.request.component.request.authorization.vo.AuthVo
+import theme
 
 @Composable
 fun RequestAuthorizationComponent(authVo: AuthVo) {
@@ -40,7 +46,21 @@ private fun AuthenticationComponentSelector(
         AuthorizationType.BEARER -> BearerAuthComponent(vo = authVo.bearer)
         AuthorizationType.API_KEY -> ApiKeyAuthComponent(vo = authVo.apiKey)
 
-        AuthorizationType.OAUTH2 -> Text("Coming soon")
+        AuthorizationType.OAUTH2 -> ComingSoonMessage()
         AuthorizationType.NONE -> {}
+    }
+}
+
+@Composable
+fun ComingSoonMessage(modifier: Modifier = Modifier) {
+    Box(
+        modifier = Modifier.fillMaxSize().border(1.dp, color = theme.colors.table.border),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "Coming soon",
+            color = theme.colors.input.label,
+            fontSize = 20.sp
+        )
     }
 }
